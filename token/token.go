7680de666f457d6f7801c2837ff84cf0c62fe8ed
hoodielive <hoodielive@gmail.com>
2019-12-02 21:14:15 -0500

    defining our tokens

--- token/token.go
+++ token/token.go
@@ -8,3 +8,29 @@ type Token struct {
 	Type    TokenType
 	Literal string
 }
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 123456
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
